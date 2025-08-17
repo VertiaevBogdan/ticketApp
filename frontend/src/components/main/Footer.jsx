890093646b9b166154d3552f818fcc235264309b
Vertiaev Bogdan <bogdan.vertiaev@olc.cz>
2025-08-17 21:57:43 +0200

    almost done

--- frontend/src/components/main/Footer.jsx
+++ frontend/src/components/main/Footer.jsx
@@ -10,10 +10,22 @@ function Footer() {
             </nav>
             <nav>
                 <ul className="inline-flex items-center space-x-11">
-                    <li><a href="https://www.youtube.com/@Spoontamer"><img className="h-7" src="../../../public/img/youtube.png" alt="YouTube"/></a></li>
-                    <li><a href="https://www.instagram.com/spoontamer/"><img className="h-7" src="../../../public/img/instagram.png" alt="Instagram"/></a></li>
-                    <li><a href="https://vk.com/spoontamer"><img className="h-10" src="../../../public/img/vk.png" alt="VK"/></a></li>
-                    <li><a href="https://twitter.com/Spoontamer"><img className=" text-red-500 fill-current h-7" src="../../../public/img/twitter.svg" alt="Twitter"/></a></li>
+                    <li><a target="_blank"
+                           rel="noopener noreferrer"
+                           href="https://www.youtube.com/@Spoontamer">
+                        <img className="h-7" src="../../../public/img/youtube.png" alt="YouTube"/></a></li>
+                    <li><a target="_blank"
+                           rel="noopener noreferrer"
+                           href="https://www.instagram.com/spoontamer/">
+                        <img className="h-7" src="../../../public/img/instagram.png" alt="Instagram"/></a></li>
+                    <li><a target="_blank"
+                           rel="noopener noreferrer"
+                           href="https://vk.com/spoontamer">
+                        <img className="h-10" src="../../../public/img/vk.png" alt="VK"/></a></li>
+                    <li><a target="_blank"
+                           rel="noopener noreferrer"
+                           href="https://twitter.com/Spoontamer">
+                        <img className="h-7" src="../../../public/img/twitter.svg" alt="Twitter"/></a></li>
                 </ul>
             </nav>
         </footer>
