c96090dc8acd8ac118cd3dfb70d265cf44899dfc
Vertiaev Bogdan <bogdan.vertiaev@olc.cz>
2025-08-18 02:38:17 +0200

    added h1 text

--- frontend/src/components/main/BackgroundVideo.jsx
+++ frontend/src/components/main/BackgroundVideo.jsx
@@ -1,16 +1,16 @@
 function BackgroundVideo() {
     return (
-        <>
+        <div class="flex  items-center justify-center">
             <video
-                className="absolute top-0 left-0 w-full h-full object-cover rounded-t-3xl"
+                className="absolute top-0 left-0 w-full h-[95vh] object-cover overflow-hidden rounded-t-3xl"
                 src="../../../public/trailer.mp4"
                 autoPlay
                 loop
                 playsInline
                 muted
             />
-            <div className="absolute top-[50%] right-0 bottom-0 left-0 bg-gradient-custom"></div>
-        </>
+            {/*<div className="absolute bottom-10 left-0 right-0 h-1/2 bg-gradient-to-t from-[#111111] to-transparent"></div>*/}
+        </div>
     );
 }
 
--- frontend/src/components/main/Main.jsx
+++ frontend/src/components/main/Main.jsx
@@ -2,8 +2,14 @@
 
 function Main() {
     return (
-        <section class="relative min-h-screen w-full">
-
+        <section class="relative min-h-screen w-full mt-[31em]">
+            <article class="flex flex-col">
+                <div class="text-white/40 ">18+</div>
+                <h1 class="text-white mt-4 text-8xl font-bold">
+                    <p class="text-6xl">СТЕНДАП СПЕШЛ</p>
+                    ВЕСЕЛАЯ<span className="text-[#801219]">/</span>ЖИЗНЬ
+                </h1>
+            </article>
         </section>
     )
 }
