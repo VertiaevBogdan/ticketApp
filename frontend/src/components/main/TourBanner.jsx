be8f23ee24c47fc6e0d6aa4643f05dde5ca328fa
Vertiaev Bogdan <bogdan.vertiaev@olc.cz>
2025-08-24 18:30:33 +0200

    tour section

--- frontend/src/components/main/TourBanner.jsx
+++ frontend/src/components/main/TourBanner.jsx
@@ -1,10 +1,12 @@
-function TourBanner() {
+ function TourBanner() {
     return (
         <section class="w-full">
-            <div class="border-top-line w-full"></div>
-            <div><p>akladhlkaldad</p></div>
+            <div class="border-top-line"></div>
+            <img src="../../../public/img/banner.png" alt="banner"
+                 className="transform transition-transform
+                 duration-300 ease-in-out hover:scale-[102%]"/>
         </section>
     );
-}
+ }
 
-export default TourBanner
+ export default TourBanner
