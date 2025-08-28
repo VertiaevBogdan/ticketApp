1d8e45b50bb615cefe464921af56cad62226436d
Vertiaev Bogdan <bogdan.vertiaev@olc.cz>
2025-08-29 00:49:10 +0200

    refreshed standup cards list

--- frontend/src/components/main/StandUpList.jsx
+++ frontend/src/components/main/StandUpList.jsx
@@ -17,24 +17,19 @@ function StandUpList() {
         <section class="w-full mb-[100px]">
             <div class="border-top-line"></div>
             <p className="text-[2.8rem] font-bold text-[#801219] mb-[1.5rem]">предыдущие стендапы</p>
-            <div className="grid grid-cols-5 items-stretch relative pl-20">
+
+            <div className="grid grid-flow-col relative"    >
               {standups.map((item) => (
-                <a key={item.id} href="#" className="cursor-pointer">
-                  <div className=" relative flex flex-col
-                                  items-center overflow-hidden
-                                  -ml-20 max-h-[90%] z-20">
-                    {/* Изображение */}
+                <a key={item.id} href="#" className="flex cursor-pointer ">
+                  <div className="relative transition-transform duration-250 hover:-translate-y-9">
                     <img
                       src={item.image}
                       alt={item.title}
-                      className="h-[25rem] flex-1 object-cover rounded-3xl"
+                      className="flex-1 object-cover rounded-3xl"
                     />
-                    {/* Полупрозрачный серый overlay */}
-                    <div className="absolute inset-0 rounded-md z-0"
-                       style={{backgroundColor: "rgba(17, 17, 17, 0.85)"}}
-                    ></div>
-                    {/* Контент поверх overlay */}
-                    <div className="absolute inset-0 p-4 flex flex-col justify-end text-white">
+                    <div className="standup_card inset-0 p-4
+                     flex flex-col justify-end text-white
+                     hover:opacity-0 transition duration-250">
                       <h2 className="text-xl font-bold">{item.title}</h2>
                       <p>{item.city}</p>
                       <p>{item.description}</p>
