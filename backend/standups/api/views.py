780812d8c30c049e43b2552b415defb192eab239
Vertiaev Bogdan <bogdan.vertiaev@olc.cz>
2025-08-28 02:32:12 +0200

    fixed cards ordering, excluded last standup in cards list

--- backend/standups/api/views.py
+++ backend/standups/api/views.py
@@ -3,8 +3,17 @@ from .serializers import StandupCardSerializer, StandupSerializer, StandupTourSe
 from ..models import Standup, StandupTour
 
 class StandupCardViewSet(viewsets.ModelViewSet):
-    queryset = Standup.objects.all().order_by('-id')
+    #queryset = Standup.objects.all().order_by('-id')
     serializer_class = StandupCardSerializer
+
+    def get_queryset(self):
+        last_standup = Standup.objects.order_by('-id').first() # первый с конца
+        qs = Standup.objects.all().order_by('-id') # сортируем
+
+        if last_standup:
+            qs = qs.exclude(id=last_standup.id) # исключаем последний
+
+        return qs
 
 class StandupTourViewSet(viewsets.ModelViewSet):
     queryset = StandupTour.objects.all().order_by('id')
