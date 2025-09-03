6ca321037438d923773f00e1da0f557f3abef71d
Vertiaev Bogdan <bogdan.vertiaev@olc.cz>
2025-09-03 19:56:43 +0200

    added imgs

--- frontend/src/components/tour/Tour.jsx
+++ frontend/src/components/tour/Tour.jsx
@@ -2,23 +2,26 @@
 
 export default function Tour() {
     return (
-        <main>
-            <div className="flex items-center justify-between text-white text-[12px] max-w-[30em]">
+        <main className="flex justify-center">
+            <div className="flex items-center justify-between text-white text-[12px] w-[35em]">
                 <div className="flex">
-                    <div className="mr-4 flex-col items-center justify-center">
+                    <div className="mr-4 p-2 flex-col items-center justify-center
+                                    bg-center bg-[url('../../public/img/tour/date-bg.png')] bg-cover
+                                     w-[42] h-[42px] rounded-[4px]">
                         <p>FEB</p>
                         <span>28</span>
                     </div>
                     <div className="text-white/40">
                         <p className="text-white">Kuta (Bali)</p>
                         fri,
-                        <span className="text-[#801219]">| </span>
+                        <span className="text-[#801219]"> | </span>
                         IC Center
                     </div>
                 </div>
                 <div>
                     <button className="cursor-pointer hover:bg-black/50">
-                        БИЛЕТЫ
+                        <img src="../../../public/img/tour/tickets.png.webp" alt=""
+                                className="max-h-[24px] rounded-[4px]"/>
                     </button>
                 </div>
             </div>
