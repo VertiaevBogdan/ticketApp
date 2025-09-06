f33c263b3d8df4902995d21189c2dd6ab413096b
Vertiaev Bogdan <bogdan.vertiaev@olc.cz>
2025-09-06 03:17:25 +0200

    rough version, will be revised

--- frontend/src/components/tour/Tour.jsx
+++ frontend/src/components/tour/Tour.jsx
@@ -14,17 +14,22 @@ export default function Tour() {
     }, [])
 
     return (
-        <body className="bg-[#0c0c0c]">
-            <main className="flex flex-col justify-center items-center">
+        <body className="bg-[#0c0c0c]
+                        bg-[url('../../public/img/tour/full-bg.png')]
+                            bg-contain bg-no-repeat" >
+            <main className="flex flex-col justify-center items-center
+                            mt-[12%]">
+                <img src="../../../public/img/tour/_813.png.webp" alt="813"
+                        className="max-w-[28em]"/>
             {tours.map((item) => (
                 <div key={item.id} className="flex items-center justify-between
                                             text-white text-[12px] w-[35em] mb-3">
                 <div className="flex">
-                    <div className="mr-4 p-2 flex-col items-center justify-center
-                                    bg-center bg-[url('../../public/img/tour/date-bg.png')] bg-cover
-                                     w-[42] h-[42px] rounded-[4px]">
-                        <p>{item.month}</p>
-                        <span>{item.month_num_day}</span>
+                    <div className="mr-4 pb-1 w-[42px] flex flex-col justify-center items-center bg-no-repeat
+                                    bg-center bg-[url('../../public/img/tour/date-bg.png')] bg-contain
+                                    h-[42px] rounded-[4px]">
+                        <p className="text-[11px] mt-1">{item.month}</p>
+                        <span className="text-lg font-bold">{item.month_num_day}</span>
                     </div>
                     <div className="text-white/40">
                         <p className="text-white">{item.city}</p>
@@ -35,8 +40,11 @@ export default function Tour() {
                 </div>
                 <div>
                     <button className="cursor-pointer hover:bg-black/50">
-                        <img src="../../../public/img/tour/tickets.png.webp" alt=""
+                        {item.sold_out ? (
+                            <img src="../../../public/img/tour/tickets.png.webp" alt="Buy a ticket"
                                 className="max-h-[24px] rounded-[4px]"/>
+                        ) : (<img src="../../../public/img/tour/Sold_out.png" alt="Sold out"
+                                  className="max-h-[52px] rounded-[4px] hover:shadow-none"/>)}
                     </button>
                 </div>
             </div>
