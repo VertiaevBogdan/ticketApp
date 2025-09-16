1525789e1ccb09d097345efd1b2b58a0bc5fd0f1
Vertiaev Bogdan <bogdan.vertiaev@olc.cz>
2025-09-17 00:52:31 +0200

    fixed bg preview

--- frontend/src/components/standups/StandupPreview.jsx
+++ frontend/src/components/standups/StandupPreview.jsx
@@ -1,12 +1,14 @@
 export default function StandupPreview({standup}) {
     return(
-        <section className="flex flex-col relative overflow-hidden">
-            <div className="absolute flex inset-0 items-center ">
+        <section className="flex flex-col relative overflow-hidden
+                            h-[100vh] mt-[-73px] mx-[-42px]">
+            <div className="absolute flex inset-0 items-center
+             bg-center bg-cover bg-no-repeat w-[1440px] "
+            style={{ backgroundImage: `url(${standup.preview})` }}>
                 <div className="bg-gradient-custom"></div>
             </div>
             <div className="grid grid-flow-col justify-between gap-[60px] items-end
-                            w-[1440px] h-[720px] bg-center bg-cover bg-no-repeat"
-                 style={{ backgroundImage: `url(${standup.preview})` }}>
+                           ">
 
             </div>
         </section>
