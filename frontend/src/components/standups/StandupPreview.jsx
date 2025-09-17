151ed2ca4bf72510e44d781f621c8c379e6ab9a6
Vertiaev Bogdan <bogdan.vertiaev@olc.cz>
2025-09-17 15:47:12 +0200

    fixed preview text

--- frontend/src/components/standups/StandupPreview.jsx
+++ frontend/src/components/standups/StandupPreview.jsx
@@ -3,13 +3,19 @@ export default function StandupPreview({standup}) {
         <section className="flex flex-col relative overflow-hidden
                             h-[100vh] mt-[-73px] mx-[-42px]">
             <div className="absolute flex inset-0 items-center
-             bg-center bg-cover bg-no-repeat w-[1440px] "
+             bg-center bg-cover  bg-no-repeat w-[1440px] "
             style={{ backgroundImage: `url(${standup.preview})` }}>
                 <div className="bg-gradient-custom"></div>
             </div>
-            <div className="grid grid-flow-col justify-between gap-[60px] items-end
-                           ">
-
+            <div className="absolute grid grid-flow-col justify-between gap-[60px] items-end
+                            bottom-0 w-full py-[clamp(20px,6vh,43px)] px-[42px]">
+                <span className="flex flex-col mb-6">
+                    <div className="text-white/40 text-[20px] font-medium">18+</div>
+                    <h1 className="mt-4 text-[96px] leading-[1.05] font-bold">
+                        <p className="text-white text-[0.665em]">СТЕНДАП</p>
+                        <span className="text-[#801219]">{standup.title}</span>
+                    </h1>
+                </span>
             </div>
         </section>
     );
