1ea90fa3c6df936bdedb3ae704e932ea2b12d6d4
Vertiaev Bogdan <bogdan.vertiaev@olc.cz>
2025-09-29 00:06:52 +0200

    added register logic on frontend

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -1,17 +1,26 @@
 import {Link} from "react-router";
-import RegisterPage from "./RegisterPage.jsx";
+import {useState} from "react";
+
 
 export default function LoginPage(){
+
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("")
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+    }
+
     return(
          <section className="flex justify-center my-[5em] p-[68px]">
-            <form className="border border-solid border-[#801219] flex flex-col
+            <form onSubmit={handleSubmit} className="border border-solid border-[#801219] flex flex-col
                             items-center justify-center text-white max-w-[320px]
                             p-[26px] rounded-3xl">
                 <h1 className="font-bold text-[1.8rem] mb-[30px] text-center">вход</h1>
-                <input type="email" placeholder="email"
+                <input type="email" value={email} placeholder="email"
                     className="text-center border-b border-solid border-white/40
                                  outline-none p-3"/>
-                <input type="password" placeholder="пароль"
+                <input type="password" value={password} placeholder="пароль"
                     className="text-center border-b border-solid border-white/40
                                  outline-none p-3"/>
                 <button className="flex items-center mb-5 bg-[#801219] p-3 cursor-pointer
--- frontend/src/pages/RegisterPage.jsx
+++ frontend/src/pages/RegisterPage.jsx
@@ -1,26 +1,95 @@
+import api from "../api/api.js";
 import {Link} from "react-router";
+import {useState} from "react";
 
 export default function RegisterPage() {
+
+    const [formData, setFormData] = useState({
+        name: "",
+        email: "",
+        password: "",
+        confirmPassword: "",
+    })
+    const [error, setError] = useState("");
+
+    const handleChange = (event) => {
+
+        const {name, value} = event.target;
+
+        setFormData({
+            ...formData,
+            [name]: value,
+        });
+
+        if (name === "confirmPassword" && value !== formData.password){
+            setError("пароли не совпадают")
+        } else if ( name === "confirmPassword" && value === formData.password) {
+            setError("");
+        }
+    };
+
+    const handleSubmit = async (event) =>{
+        event.preventDefault();
+
+        if (formData.password !== formData.confirmPassword) {
+            setError("пароли не совпадают");
+            return;
+        }
+
+        try {
+            await api.post("/users/register", {
+                name: formData.name,
+                email: formData.email,
+                password: formData.password,
+            });
+        } catch (err) {
+            console.error(err);
+        }
+    }
+
     return(
         <section className="flex justify-center p-[68px] my-[2.5em]">
-            <form className="border border-solid border-[#801219] flex flex-col
+            <form onSubmit={handleSubmit} className="border border-solid border-[#801219] flex flex-col
                             items-center justify-center text-white max-w-[320px]
                             p-[26px] rounded-3xl">
                 <h1 className="font-bold text-[1.8rem] mb-[30px] text-center">регистрация</h1>
-                <input type="text" placeholder="имя"
+                <input
+                    type="text"
+                    name="name"
+                    placeholder="имя"
+                    value={formData.name}
+                    onChange={handleChange}
                        className="text-center border-b border-solid border-white/40
                                  outline-none p-3"/>
-                <input type="email" placeholder="email"
+                <input
+                    type="email"
+                    name="email"
+                    placeholder="email"
+                    value={formData.email}
+                    onChange={handleChange}
                     className="text-center border-b border-solid border-white/40
                                  outline-none p-3"/>
-                <input type="password" placeholder="пароль"
+                <input
+                    type="password"
+                    name="password"
+                    placeholder="пароль"
+                    value={formData.password}
+                    onChange={handleChange}
                     className="text-center border-b border-solid border-white/40
                                  outline-none p-3"/>
-                <input type="password" placeholder="повторить пароль"
+                <input
+                    type="password"
+                    name="confirmPassword"
+                    value={formData.confirmPassword}
+                    onChange={handleChange}
+                    placeholder="повторить пароль"
                     className="text-center border-b border-solid border-white/40
                                  outline-none p-3"/>
+                {error && <p className="text-[#801219] text-[16px] mt-3">{error}</p>}
                 <Link to="/register">
-                    <button className="flex items-center mb-5 bg-[#801219] p-3 cursor-pointer
+                    <button
+                        type="submit"
+                        className="flex items-center mb-5 bg-[#801219] p-3 cursor-pointer
                                     rounded-lg h-9 w-[258px] justify-center mt-4 text-[17px]
                                     hover:bg-[#951b22] transition duration-200">
                     зарегестрироваться
